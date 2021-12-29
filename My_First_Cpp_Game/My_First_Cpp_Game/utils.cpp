d150af15a1a108f5fea6a3dcfb0a94dfb9ebd52c
Bedirhan <53793285+bedirhanbuyukoz@users.noreply.github.com>
2021-12-29 22:30:57 +0300

    Add files via upload

---
+++ My_First_Cpp_Game/My_First_Cpp_Game/utils.cpp
@@ -0,0 +1,24 @@
+
+//IMPORTS
+typedef char s8;
+typedef unsigned char u8;
+
+
+typedef short s16;
+typedef unsigned short u16;
+typedef int s32;
+typedef unsigned int u32;
+typedef long long s64;
+typedef unsigned long long u64;
+
+#define global_variable static
+#define internal static
+
+
+//Belirtilen girdinin her bileþenini önceden tanýmlanmýþ bir aralýkta kelepçelemeler
+inline int
+clamp(int min, int val, int max) {
+	if (val < min) return min;
+	if (val > max) return max;
+	return val;
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Pong-Game-Project
+Creating Ui with C++
+
+
+
+
+
+https://user-images.githubusercontent.com/53793285/147059807-7a6a29af-d9cf-4aa8-a21d-4b71bfe2122a.mp4
+
+
+
+
+
+https://user-images.githubusercontent.com/53793285/147060516-f9deab1b-580f-452b-8fb4-d076e803f593.mp4
+
